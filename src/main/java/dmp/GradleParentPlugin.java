99d87b41f4073b4f1ddbbced83a05db32beb75bc
Giray Kocacinar <Giray.Kocacinar@fonetbt.com>
2022-01-18 14:22:25 +0300

    son hali

---
+++ src/main/java/dmp/GradleParentPlugin.java
@@ -0,0 +1,13 @@
+package dmp;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class GradleParentPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+
+    }
+
+
+}
